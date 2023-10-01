158aa83c43798045c7861028caca68eeb52eb5df
ANGELINA MKRTCHYAN <angie@ANGELINAs-MacBook-Air.local>
2023-10-01 14:02:22 +0100

    [D] now model.fit processes first batch of 4 traning imgs, then starts 2nd batch and fails with 'Graph execution error'

--- StepByStep/SecondStep_DataGenerator.py
+++ StepByStep/SecondStep_DataGenerator.py
@@ -6,14 +6,20 @@ import pydicom
 import cv2
 import gc
 import random
+from collections import Counter
+
 
 height = 3518  # The height of your images
 width = 2800  # The width of your images
 num_features = 2  # Number of feature columns, here you have 'laterality' and 'view_position'
 
-available_image_ids = []
 preloaded_image_ids = []
 used_image_ids = []
+
+
+mapping_dict_density = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
+mapping_dict_laterality = {'left': 0, 'right': 1}
+mapping_dict_view_position = {'CC': 0, 'MLO': 1}
 
 
 def conditional_resize(img, img_id, target_height=height, target_width=width):
@@ -82,7 +88,7 @@ def preload_images(df, num_preloaded):
     preloaded_images = {}
 
     # Filter the DataFrame to exclude rows with image_id in preloaded_image_ids
-    filtered_df = df[~df['image_id'].isin(preloaded_image_ids)]
+    filtered_df = df[~df['image_id'].isin(preloaded_image_ids) & ~df['image_id'].isin(used_image_ids)]
 
     # Shuffle the filtered DataFrame and select the first num_preloaded rows
     shuffled_df = filtered_df.sample(frac=1).reset_index(drop=True)
@@ -99,63 +105,63 @@ def preload_images(df, num_preloaded):
     print("preload images completed")
     return preloaded_images
 
-mapping_dict_density = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
-mapping_dict_laterality = {'left': 0, 'right': 1}
-mapping_dict_view_position = {'CC': 0, 'MLO': 1}
 
-def my_data_generator(df, batch_size, preloaded_images, feature_input):
-    print('\n =================\n',
-          'entered my_data_generator'.upper(),
-          '\n====================')
+def get_sample_weights(y):
+    # Flatten the array back to a 1D array before using Counter
+    y_flat = y.flatten()
+    counter = Counter(y_flat)
+    max_val = float(max(counter.values()))
+    sample_weights = np.array([max_val / counter[i] for i in y_flat])
+    return sample_weights
 
-    filtered_df = df[df['image_id'].isin(preloaded_images.keys())]
-    unused_indices = np.arange(len(filtered_df))
+
+def my_data_generator_two(
+        batch_size,
+        mode # training or validation
+):
+    print("I'm in data_generator. Current mode is: ", mode, "\n")
+
+    data = training_data
+    if mode == "validation": data = validation_data
+    elif mode == "test": data = test_data
+
+
+    batch_images = np.zeros((batch_size, height, width), dtype=np.float32)  # Adjust dtype as needed
+    batch_labels_birads = np.zeros((batch_size, 1), dtype=int)  # Assuming birads labels are integers
+    batch_labels_density = np.zeros((batch_size, 1), dtype=int)  # Assuming density labels are integers
+    batch_features = np.zeros((batch_size, num_features), dtype=int)  # Assuming features are integers
+    laterality_feature = np.zeros((batch_size, num_features), dtype=int)  # Assuming features are integers
+    position_feature = np.zeros((batch_size, num_features), dtype=int)  # Assuming features are integers
+    batch_weights_birads = np.zeros((batch_size,), dtype=np.float32)  # Assuming weights are float numbers
+    batch_weights_density = np.zeros((batch_size,), dtype=np.float32)  # Assuming weights are float numbers
 
     while True:
-        if len(unused_indices) < batch_size:
-            # Reset unused_indices if there are not enough for a batch
-            unused_indices = np.arange(len(filtered_df))
+        print("my_data_generator_two -> while True")
+        preloaded_images = preload_images(data, num_preloaded=batch_size)
+        for i, image_id in enumerate(preloaded_images):
+            resized_preloaded_image = conditional_resize(preloaded_images[image_id],image_id)
+            batch_images[i] = resized_preloaded_image
+            batch_labels_birads[i, 0] = get_data_from_csv(image_id, mode, 'breast_birads') - 1
+            batch_labels_density[i, 0] = mapping_dict_density.get(get_data_from_csv(image_id, mode, 'breast_density')) -1
+            laterality_feature[i] = mapping_dict_laterality.get(get_data_from_csv(image_id, mode,'laterality'), -1),
+            position_feature[i] = mapping_dict_view_position.get(get_data_from_csv(image_id, mode,'view_position'), -1)
+            # batch_features[i] = [mapping_dict_laterality.get(get_data_from_csv(image_id, mode,'laterality'), -1),
+            #                      mapping_dict_view_position.get(get_data_from_csv(image_id, mode,'view_position'), -1)]
 
-        batch_indices = np.random.choice(a=unused_indices, size=batch_size, replace=False)
-        batch_df = filtered_df.iloc[batch_indices]
+            preloaded_image_ids.remove(image_id)
+            used_image_ids.append(image_id)
 
-        # Remove used indices from unused_indices
-        unused_indices = np.setdiff1d(unused_indices, batch_indices)
+        reshaped_labels_birads = batch_labels_birads.reshape(-1, 1)
+        reshaped_labels_density = batch_labels_density.reshape(-1, 1)
 
-        # Initialize your arrays
-        batch_images = np.zeros((batch_size, height, width), dtype=np.float32)  # Adjust dtype as needed
-        batch_labels_birads = np.zeros((batch_size, 1), dtype=int)  # Assuming birads labels are integers
-        batch_labels_density = np.zeros((batch_size, 1), dtype=int)  # Assuming density labels are integers
-        batch_features = np.zeros((batch_size, num_features), dtype=int)  # Assuming features are integers
-        batch_weights_birads = np.zeros((batch_size,), dtype=np.float32)  # Assuming weights are float numbers
-        batch_weights_density = np.zeros((batch_size,), dtype=np.float32)  # Assuming weights are float numbers
+        batch_weights_birads = get_sample_weights(reshaped_labels_birads.flatten())
+        batch_weights_density = get_sample_weights(reshaped_labels_density.flatten())
 
-        for i, original_idx in enumerate(batch_indices):
-            print("\n i'm in FOR statement:", i, original_idx)
-            row = batch_df.iloc[i]
-            img_id = row['image_id']
-            # if img_id in preloaded_images:
-            img = preloaded_images[img_id]
-            img = conditional_resize(img, img_id)
-            # else:
-            #     study_id = row['study_id']
-            #     img_path = f"/content/drive/MyDrive/Colab/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0/images/{study_id}/{img_id}.dicom"
-            #     img = pydicom.dcmread(img_path).pixel_array
-            #     img = conditional_resize(img, img_id)
-
-            # img = conditional_resize(img, img_id)
-            batch_images[i] = img
-            batch_labels_birads[i, 0] = row['breast_birads'] - 1
-            batch_labels_density[i, 0] = mapping_dict_density.get(row['breast_density'], -1)
-            batch_features[i] = [mapping_dict_laterality.get(row['laterality'], -1),
-                                 mapping_dict_view_position.get(row['view_position'], -1)]
-            batch_weights_birads[i] = sample_weights_birads[original_idx]
-            batch_weights_density[i] = sample_weights_density[original_idx]
-        #     WAITS changed their type from float32 to float64?
-
-        return {
+        yield ({
             'image_input': batch_images,
-            'feature_input': batch_features
+            'laterality_input': laterality_feature,
+            'position_input': position_feature
+            # 'feature_input': batch_features
         }, {
             'birads_output': batch_labels_birads,
             'density_output': batch_labels_density
@@ -163,46 +169,7 @@ def my_data_generator(df, batch_size, preloaded_images, feature_input):
             'birads_output': batch_weights_birads,
             'density_output': batch_weights_density
         }
-
-
-def my_data_generator_two(
-        preloaded_images,
-        sample_weights, # from 3rd step: sample_weights_train
-        batch_size,
-        mode # training or validation
-):
-    batch_images = np.zeros((batch_size, height, width), dtype=np.float32)  # Adjust dtype as needed
-    batch_labels_birads = np.zeros((batch_size, 1), dtype=int)  # Assuming birads labels are integers
-    batch_labels_density = np.zeros((batch_size, 1), dtype=int)  # Assuming density labels are integers
-    batch_features = np.zeros((batch_size, num_features), dtype=int)  # Assuming features are integers
-    batch_weights_birads = np.zeros((batch_size,), dtype=np.float32)  # Assuming weights are float numbers
-    batch_weights_density = np.zeros((batch_size,), dtype=np.float32)  # Assuming weights are float numbers
-
-    while True:
-        for i, image_id in enumerate(preloaded_images):
-            resized_preloaded_image = conditional_resize(preloaded_images[image_id],image_id)
-            batch_images[i] = resized_preloaded_image
-            batch_labels_birads[i, 0] = get_data_from_csv(image_id, mode, 'breast_birads') - 1
-            batch_labels_density[i, 0] = mapping_dict_density.get(get_data_from_csv(image_id, mode, 'breast_density')) -1
-            batch_features[i] = [mapping_dict_laterality.get(get_data_from_csv(image_id, mode,'laterality'), -1),
-                                 mapping_dict_view_position.get(get_data_from_csv(image_id, mode,'view_position'), -1)]
-
-        batch_weights_birads = sample_weights['birads_output']
-        batch_weights_density = sample_weights['density_output']
-        # weights = [batch_weights_birads, batch_weights_density]
-
-        return ({
-            'image_input': batch_images,
-            'feature_input': batch_features
-        }, {
-            'birads_output': batch_labels_birads,
-            'density_output': batch_labels_density
-        }
-        # , {
-        #     weights
-        # }
-        , {
-            'birads_weights': batch_weights_birads,
-            'density_weights': batch_weights_density
-        }
         )
+
+        print("\n preloaded_image_ids:", preloaded_image_ids)
+        print("used_image_ids:", used_image_ids, "\n")
